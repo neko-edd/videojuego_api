60ff46438abdfbffe3997832e5fda058d88c8ccc
eduneko22 <edupallares22@gmail.com>
2026-01-05 19:46:12 +0100

    task_models.mjs SUBIDO

---
+++ API_REST/models/task_models.mjs
@@ -0,0 +1,28 @@
+export class User{
+    constructor({user_name, password}){
+        this.user_name = user_name
+        this.password = password
+    }
+}
+export class Favourites{
+    constructor({user_name, name_game, price}){
+        this.user_name = user_name
+        this.name_game = name_game
+        this.price = price
+    }
+}
+/*export class Sales{
+    constructor({user_name, name_game, price, offer}){
+        this.user_name = user_name
+        this.name_game = name_game
+        this.price = price
+        this.offer = offer
+    }
+}*/
+export class Games{
+    constructor({name_game, price, description}){
+        this.name_game = name_game
+        this.price = price
+        this.description = description
+    }
+}
