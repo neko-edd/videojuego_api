5c25c2ff8f6bcc4cef853a5db692c08b09b5e396
eduneko22 <edupallares22@gmail.com>
2026-01-08 02:31:36 +0100

    CREACION DE METODO PARA CREAR LAS TABLAS EN SUPABASE postgre

--- API_REST/app.mjs
+++ API_REST/app.mjs
@@ -4,7 +4,7 @@ import express from "express"
 import session from "express-session";
 import cors from "cors";
 import routerLogin from "./routes/task_routes.mjs"
-//import { log } from "./middleware/loggin.mjs";
+import {createTables} from "./configuration/database.mjs"
 
 const PORT = 60000;
 const app = express();
@@ -20,18 +20,29 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-//app.use(log);
-//USAR EL IMPORT CREADO
+
 app.use(session({
     secret: "Cadena secreta",
     resave: false,
     saveUninitialized: false,
     cookie: { maxAge: 1000 * 60 * 30 } // 30 min
 }));
-app.use(routerLogin); //TODAS LAS RUTAS DE DATOS TIENEN DELANTE /api (TE OBLIGA-RESTRICCION) 
+app.use(routerLogin); 
+
+async function loadTables(){
+    try{
+
+    await createTables()
+
+    }catch(err){
+    console.error("Error al iniciar el servidor:", err);
+    process.exit(1);
+    }
+}
+
+loadTables()
 
 app.listen(PORT, ()=>console.log("INICIAMOS", PORT))
 
-//PARA EL CLIENTE
-/*app.set("view engine", "ejs")
-app.set("views", path.join(path.resolve("."), "uso_vistas", "views"))*/
+
+
--- API_REST/configuration/database.mjs
+++ API_REST/configuration/database.mjs
@@ -1,36 +1,14 @@
-/*import {Pool} from "pg"
-
-const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT),
-    host: process.env.DB_HOST,
-    max: 20 
-
-    //PODEMOS INCLUIR MAS INFORMACION
-})
-
-export default pool*/
-/*import 'dotenv/config';
-import pkg from "pg";
-const { Pool } = pkg;
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  max: 20,
-  ssl: { rejectUnauthorized: false } // necesario para Supabase
-});
-
-export default pool;*/
 import dotenv from 'dotenv';
 import path from 'path';
 import pkg from 'pg';
 const { Pool } = pkg;
 
+
+
+
 dotenv.config({ path: path.resolve('./configuration/.env') });
 
-console.log("DATABASE_URL:", process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_NAME, process.env.DB_PORT, process.env.DB_HOST);
+//console.log("DATABASE_URL:", process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_NAME, process.env.DB_PORT, process.env.DB_HOST); => COMPROBAR DATOS SIN undefined
 
 const pool = new Pool({
     user: process.env.DB_USER,
@@ -43,4 +21,53 @@ const pool = new Pool({
     //PODEMOS INCLUIR MAS INFORMACION
 })
 
-export default pool;
+export async function createTables() {
+    try {
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS usuarios (
+                id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                user_name VARCHAR(255) UNIQUE NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        `);
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS juegos (
+                id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                name_game VARCHAR(255) UNIQUE NOT NULL,
+                price NUMERIC(10, 2) NOT NULL,
+                image TEXT,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        `);
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS favoritos (
+                id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                user_id UUID NOT NULL,
+                game_id UUID NOT NULL,
+                price NUMERIC(10, 2) NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (user_id) REFERENCES usuarios(id) ON DELETE CASCADE,
+                FOREIGN KEY (game_id) REFERENCES juegos(id) ON DELETE CASCADE,
+                UNIQUE(user_id, game_id)
+            );
+        `);
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS ventas (
+                id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                user_id UUID NOT NULL,
+                game_id UUID NOT NULL,
+                price NUMERIC(10, 2) NOT NULL,
+                offer BOOLEAN DEFAULT FALSE,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (user_id) REFERENCES usuarios(id) ON DELETE CASCADE,
+                FOREIGN KEY (game_id) REFERENCES juegos(id) ON DELETE CASCADE
+            );
+        `);     
+    } catch (err) {
+        console.error("Error al crear tablas:", err.message);
+        throw err;
+    }
+}
+
+export default pool
